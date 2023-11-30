c408a3ed485a15c673d4d547a58ae94efe134dcb
qcloud <ubuntu@localhost.localdomain>
2023-11-30 23:31:06 +0800

    reminder views.py

--- backend/reminder/views.py
+++ backend/reminder/views.py
@@ -7,15 +7,21 @@ from datetime import datetime
 
 #@require_http_methods(["POST"])
 def set_reminder(request):
-    try:
-        data = json.loads(request.body)
-        user_id = data['user_id']
-        reminder_time = datetime.fromisoformat(data['reminder_time'])
-        message = data['message']
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            user_id = data['user_id']
+            reminder_time = datetime.fromisoformat(data['reminder_time'])
+            message = data['message']
 
-        reminder = Reminder(user_id=user_id, reminder_time=reminder_time, message=message)
-        reminder.save()
+            reminder = Reminder(user_id=user_id, reminder_time=reminder_time, message=message)
+            reminder.save()
 
-        return JsonResponse({"status": "success"})
-    except Exception as e:
-        return JsonResponse({"status": "error", "message": str(e)})
+            return JsonResponse({"status": "success"})
+        except Exception as e:
+            return JsonResponse({"status": "error", "message": str(e)})
+    elif request.method == 'GET':
+        # 添加一些针对 GET 请求的逻辑
+        return JsonResponse({"message": "This endpoint requires a POST request."})
+    else:
+        return JsonResponse({"message": "Invalid request method."})
