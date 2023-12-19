d2adba9d9c496b22fba5a6e28cc7ad19c18da8c4
azishabibi <azis2238437609@gmail.com>
2023-12-19 21:23:11 +0800

    finished clockin and report, report is now done under clockin

--- backend/clockin/models.py
+++ backend/clockin/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from user.models import User
+from django.utils import timezone
+import pytz
 
+beijing_tz = pytz.timezone('Asia/Shanghai')
+timezone.activate(beijing_tz)
 class OrthodonticCheckIn(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField()
     check_in_type = models.CharField(max_length=20)  # 打卡类型：'摘下牙套' off 或 '带上牙套'on
 
     def __str__(self):
-        return f"{self.user.username} - {self.date} - {self.check_in_type}"
+        return f"{self.user.nickname} - {self.date} - {self.check_in_type}"
