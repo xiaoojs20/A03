9ce297cc9512cf207518d18bcc62c694730b3b73
azishabibi <azis2238437609@gmail.com>
2023-12-21 00:14:22 +0800

    add YY.urls.py

--- backend/notifications/tests.py
+++ backend/notifications/tests.py
@@ -6,28 +6,23 @@ import json
 class NotificationTests(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_id = 'test_user'
+        SystemNotification.objects.create(user_id=self.user_id, message='Test Notification')
 
+    def test_get_system_notifications(self):
+        url = reverse('get_system_notifications', kwargs={'user_id': self.user_id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        notifications = json.loads(response.content)
+        self.assertEqual(len(notifications), 1)
+        self.assertEqual(notifications[0]['message'], 'Test Notification')
     def test_create_notification(self):
-        # 测试创建通知
         url = reverse('create_notification')
         data = {
-            'user_id': 'test_user',
+            'user_id': self.user_id,
             'message': 'This is a test notification'
         }
         response = self.client.post(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(SystemNotification.objects.count(), 1)
-        self.assertEqual(SystemNotification.objects.first().message, 'This is a test notification')
-
-    def test_get_system_notifications(self):
-        # 先创建一个通知
-        SystemNotification.objects.create(user_id='test_user', message='Test Notification')
-
-        # 测试获取通知
-        url = reverse('notifications:get_system_notifications', kwargs={'user_id': 'test_user'})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
-        notifications = json.loads(response.content)
-        self.assertEqual(len(notifications), 1)
-        self.assertEqual(notifications[0]['message'], 'Test Notification')
+        self.assertEqual(SystemNotification.objects.count(), 2)  # Assuming one was already created in setUp
+        self.assertEqual(SystemNotification.objects.last().message, 'This is a test notification')
