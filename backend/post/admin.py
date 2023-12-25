8eeda24bedc4cc4a3768d3f1308fa0aba21afe32
xiaoojs20 <xiaoojs20@gmail.com>
2023-12-25 20:01:52 +0800

    add comment

--- backend/post/admin.py
+++ backend/post/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-  list_display = ['post_id', 'title', 'content']
+  list_display = ['post_id', 'publish_date', 'title', 'content']
+
+class CommentAdmin(admin.ModelAdmin):
+  list_display = ['post_id', 'comment_date', 'user', 'detail']
 
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
