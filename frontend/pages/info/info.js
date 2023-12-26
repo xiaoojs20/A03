a12d1de7e4857e2a1bdb03abdad35819e93fdc17
travelingturtle <travelturtle@qq.com>
2023-12-26 10:59:49 +0800

    update getImage

--- frontend/pages/info/info.js
+++ frontend/pages/info/info.js
@@ -5,8 +5,17 @@ Page({
     avatarUrl: '',
   },
 
+  // 每次进入更新头像等
+  onShow() {
+    console.log("onShow");
+    this.getImage(); // 在onShow生命周期函数中调用getImage函数
+    this.setData({
+      nickname: getApp().globalData.nickname,
+    });
+  },
+
   // 进入编辑个人信息主页
-  handleEditInfo(){
+  handleEditInfo() {
     wx.navigateTo({
       url: '/pages/edituserinfo/edituserinfo'
     });
@@ -14,30 +23,33 @@ Page({
   },
 
   // 获取头像
-  getImage() {
-        wx.request({
-          url: 'http://43.143.205.76:8000/user/get_image',
-          data: {
-            user_id: getApp().globalData.userid,
-          },
-          success: (res) => {
-            // 请求成功时的回调
-            console.log(res.data); // 输出返回的数据
-          },
-          fail: (err) => {
-            // 请求失败时的回调
-            console.error('请求失败', err);
-          }
-        });
-     },
+  getImage() {
+    // 下载文件
+    wx.downloadFile({
+      url: 'http://43.143.205.76:8000/user/get_image?user_id=' + getApp().globalData.userid,
+      success: (downloadRes) => {
+        if (downloadRes.statusCode === 200) {
+          // 下载成功
+          console.log(downloadRes);
+          const filePath = downloadRes.tempFilePath;
+          this.setData({
+            avatarUrl: filePath,
+          })
+        } else {
+          console.error('下载文件失败', downloadRes);
+        }
+      },
+      fail: (err) => {
+        console.error('下载文件失败', err);
+      }
+    });
+  },
 
   onLoad: function () {
-    console.log('onLoad');
     //调用应用实例的方法获取全局数据
     this.getImage();
     this.setData({
       nickname: getApp().globalData.nickname,
-      avatarUrl: getApp().globalData.avatarUrl,
-    })
+    });
   },
 })
