871bde72a02f4d44313df7723a1c3382c2ae5443
任铭飞 <rmf20@mails.tsinghua.edu.cn>
2023-12-26 20:44:00 +0800

    完善评论功能

--- frontend/pages/forum/create-post/create-post.js
+++ frontend/pages/forum/create-post/create-post.js
@@ -1,10 +1,8 @@
-// pages/forum/create-post/create-post.ts
 Page({
-
   data: {
     title: '',
     content: '',
-    imageList: []  // 已有的定义
+    imageList: []  // 存储选择的图片路径
   },
 
   inputTitle(e) {
@@ -19,65 +17,19 @@ Page({
     });
   },
 
-  
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  },
-  data: {
-    imageList: []  // 用于存储选择的图片路径
+  chooseImage() {
+    let that = this;
+    wx.chooseImage({
+      count: 3,  // 允许选择的图片数量
+      sizeType: ['compressed'],  // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'],  // 可以指定来源是相册还是相机，默认二者都有
+      success(res) {
+        const images = res.tempFilePaths;
+        that.setData({
+          imageList: that.data.imageList.concat(images)
+        });
+      }
+    });
   },
 
   submitPost() {
@@ -89,12 +41,17 @@ Page({
       });
       return;
     }
-
-    // 调用后端API
-    this.createPost(title, content, imageList);
+  
+    if (imageList.length > 0) {
+      // 如果有图片，上传图片
+      this.uploadImages(imageList, title, content);
+    } else {
+      // 如果没有图片，直接发布帖子
+      this.createTextPost(title, content);
+    }
   },
 
-  createPost(title, content, imageList) {
+  createTextPost(title, content) {
     wx.request({
       url: 'http://43.143.205.76:8000/post/create_post/',
       method: 'POST',
@@ -102,31 +59,28 @@ Page({
         'content-type': 'application/x-www-form-urlencoded'
       },
       data: {
-        user_id: "o-Hbd6bbDxfCqNpz5xsTgMLKDR3Q", // getApp().globalData.userid,
+        user_id: "o-Hbd6bbDxfCqNpz5xsTgMLKDR3Q",  // 你的用户ID
         title: title,
-        content: content,
-        imageList: imageList 
+        content: content
       },
-      success: (res) => {
-        if (res.statusCode === 200 && res.data && res.data.msg === 'create_post ok') {
-          console.log('帖子创建成功:', res.data);
+      success(res) {
+        if (res.statusCode === 200) {
           wx.showToast({
             title: '帖子发布成功',
             icon: 'success'
           });
           wx.navigateTo({
-            url: '/pages/forum/index/index',
+            url: '/pages/forum/index/index' 
           });
+          // 你可能还需要执行一些操作，比如导航到帖子列表页面
         } else {
-          console.error('帖子创建失败:', res.data);
           wx.showToast({
             title: '帖子发布失败',
             icon: 'none'
           });
         }
       },
-      fail: function(err) {
-        console.error('网络请求失败:', err);
+      fail() {
         wx.showToast({
           title: '网络请求失败',
           icon: 'none'
@@ -134,53 +88,52 @@ Page({
       }
     });
   },
-  /*
-  createPost(title, content, imageList) {
-    wx.request({
-    url: 'http://43.143.205.76:8000/post/create_post/',
-    method: 'GET',
-    data: {
-      user_id: "o-Hbd6bbDxfCqNpz5xsTgMLKDR3Q",//getApp().globalData.userid,
-      title: title,
-      content: content
-      // 可以添加更多的数据，比如图片URLs
-    },
-    success: (res) => {
-      // 检查返回的状态码或者数据中的特定字段
-      if (res.statusCode === 200 && res.data && res.data.msg === 'create_post ok') {
-        console.log('帖子创建成功:', res.data);
-        wx.showToast({
-          title: '帖子发布成功',
-          icon: 'success'
-        });
-        wx.navigateTo({
-          url: '/pages/forum/index/index',
-        })
-        // 需要重置表单或跳转到其他页面
-      } else {
-        console.error('帖子创建失败:', res.data);
-        wx.showToast({
-          title: '帖子发布失败',
-          icon: 'none'
-        });
-      }
-    },
-    */ 
+  
+  
 
-  chooseImage() {
-    
-    let that = this;
-    wx.chooseImage({
-      count: 3,  // 允许选择的图片数量
-      sizeType: ['compressed'],  // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'],  // 可以指定来源是相册还是相机，默认二者都有
-      success(res) {
-        // 返回选定照片的本地文件路径列表
-        const images = res.tempFilePaths;
-        that.setData({
-          imageList: that.data.imageList.concat(images)
-        });
-      }
-    })
+  uploadImages(imageList, title, content) {
+    for (let i = 0; i < imageList.length; i++) {
+      wx.uploadFile({
+        url: 'http://43.143.205.76:8000/post/create_post/',
+        filePath: imageList[i],
+        name: `picture_${i + 1}`,
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        formData: {
+          user_id: "o-Hbd6bbDxfCqNpz5xsTgMLKDR3Q",
+          title: title,
+          content: content
+        },
+        success: (res) => {
+          if (res.statusCode === 200) {
+            wx.showToast({
+              title: '帖子发布成功',
+              icon: 'success'
+            });
+            // 你可能还需要执行一些操作，比如导航到帖子列表页面
+          } else {
+            wx.showToast({
+              title: '帖子发布失败',
+              icon: 'none'
+            });
+          }
+        },
+        fail: (err) => {
+          console.error('图片上传失败', err);
+          // 上传失败的逻辑处理
+        }
+      });
+    }
   },
-})
+
+  // 生命周期函数
+  onLoad() {},
+  onReady() {},
+  onShow() {},
+  onHide() {},
+  onUnload() {},
+  onPullDownRefresh() {},
+  onReachBottom() {},
+  onShareAppMessage() {}
+});
--- frontend/pages/forum/post-detail/post-detail.js
+++ frontend/pages/forum/post-detail/post-detail.js
@@ -3,7 +3,8 @@ Page({
   data: {
     post: {}, // 帖子数据
     isLiked: false, // 初始未点赞状态
-    comments: [] // 评论数据，可根据需要从后端获取
+    comments: [], // 评论数据，可根据需要从后端获取
+    commentInput: ''
   },
 
   onLoad(options) {
@@ -14,12 +15,15 @@ Page({
 
   loadPostDetail(postId) {
     const that = this;
+    // 获取帖子详情
     wx.request({
-      url: 'http://43.143.205.76:8000/post/get_post_by_postid/', // 后端接口地址
+      url: 'http://43.143.205.76:8000/post/get_post_by_postid/',
       data: { post_id: postId },
       success: (res) => {
         if (res.statusCode === 200 && res.data && res.data.msg === 'get_post_by_postid ok') {
           that.setData({ post: res.data.post_info });
+          // 获取帖子的评论
+          that.loadComments(postId);
         } else {
           console.error('获取帖子详情失败:', res);
         }
@@ -29,10 +33,40 @@ Page({
       }
     });
   },
+  
+  // 新增函数用于加载评论
+  async loadComments(postId) {
+    const that = this;
+    wx.request({
+      url: 'http://43.143.205.76:8000/post/get_n_latest_comments/',
+      data: { post_id: postId, n: 10 },
+      success: async (res) => {
+        if (res.statusCode === 200 && res.data && res.data.msg === 'get_n_latest_comments ok') {
+          const commentsWithAuthor = await Promise.all(res.data.posts.map(async (comment) => {
+            try {
+              const authorNickname = await that.getUserInfo(comment.user_id);
+              const cleanedDetail = extractCommentContent(comment.detail);
+              return { ...comment, author: authorNickname, detail: cleanedDetail };
+            } catch (error) {
+              console.error(error);
+              return { ...comment, author: '未知用户', detail: extractCommentContent(comment.detail) };
+            }
+          }));
+  
+          that.setData({ comments: commentsWithAuthor });
+        } else {
+          console.error('获取评论失败:', res);
+        }
+      },
+      fail: (err) => {
+        console.error('请求评论失败', err);
+      }
+    });
+  },
 
   submitComment: function() {
     const that = this;
-    const currentUserId = 'o-Hbd6bbDxfCqNpz5xsTgMLKDR3Q'; // 替换为获取当前用户ID的方法
+    const currentUserId = 'o-Hbd6bbDxfCqNpz5xsTgMLKDR3Q'; 
     const postId = this.data.post.post_id; // 从帖子信息中获取 post_id
     const commentDetail = this.data.commentInput; // 从页面数据中获取评论内容
 
@@ -52,6 +86,9 @@ Page({
         user_id: currentUserId,
         post_id: postId,
         detail: commentDetail
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
       },
       success: function(res) {
         if (res.statusCode === 200) {
@@ -83,7 +120,7 @@ Page({
     };
   },
   navigateToPersonalInfo: function() {
-    const userId = this.data.post.user_id; // 从帖子信息中获取 user_id
+    const userId = this.data.post.user_id; //获取 user_id
     if (userId) {
       wx.navigateTo({
         url: '/pages/forum/personal_info/personal_info?user_id=' + userId
@@ -92,5 +129,28 @@ Page({
       console.error('用户ID未找到');
     }
   },
-  // 其他函数...
+  getUserInfo(userId) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: 'http://43.143.205.76:8000/user/get_info',
+        data: { user_id: userId },
+        success: (res) => {
+          if (res.statusCode === 200) {
+            resolve(res.data.nickname);  // 返回昵称
+          } else {
+            reject('无法获取用户信息');
+          }
+        },
+        fail: (err) => {
+          reject('请求失败: ' + err);
+        }
+      });
+    });
+  },
+  
 });
+
+function extractCommentContent(rawContent) {
+  const match = rawContent.match(/\('([^']*)',\)/);
+  return match ? match[1] : rawContent;
+}
