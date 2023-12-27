76f319e1799f2fdb27d94e5e79ae6b1ffc4d1e66
travelingturtle <travelturtle@qq.com>
2023-12-27 10:22:17 +0800

    update msg

--- frontend/pages/message/message.js
+++ frontend/pages/message/message.js
@@ -19,6 +19,19 @@ Page({
       success: (res) => {
         if (res.statusCode === 200) {
           console.log("系统通知数据获取成功", res.data); // 在这里处理系统通知数据
+          for (var i = 0; i < res.data.length; i++) {
+            // 创建一个 Date 对象
+            var date = new Date(res.data[i].created_at);
+            // 获取年、月、日、小时和分钟
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1; // 月份是从 0 开始计数的，所以要加 1
+            var day = date.getDate();
+            var hour = date.getHours();
+            var minute = date.getMinutes();
+            // 格式化成需要的形式
+            var formattedTime = year + '-' + (month < 10 ? '0' : '') + month + '-' + (day < 10 ? '0' : '') + day + ' ' + (hour < 10 ? '0' : '') + hour + ':' + (minute < 10 ? '0' : '') + minute;
+            res.data[i].created_at = formattedTime;
+          };
           if (res.data.length !== 0)
           {
             this.setData({
@@ -33,5 +46,19 @@ Page({
         console.error('请求失败', err); // 在这里处理请求失败的情况 
       }
     });
-  }
+  },
+
+  formatTime(originalTime){
+    // 创建一个 Date 对象
+    var date = new Date(originalTime);
+    // 获取年、月、日、小时和分钟
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1; // 月份是从 0 开始计数的，所以要加 1
+    var day = date.getDate();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    // 格式化成需要的形式
+    var formattedTime = year + '-' + (month < 10 ? '0' : '') + month + '-' + (day < 10 ? '0' : '') + day + ' ' + (hour < 10 ? '0' : '') + hour + ':' + (minute < 10 ? '0' : '') + minute;
+    return {formattedTime};
+  },
 })
